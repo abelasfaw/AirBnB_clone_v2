94f0b30abac35c05e5fc837e39f77739a52e4cc9
abelasfaw <abeloman89@gmail.com>
2022-12-25 12:56:25 -0800

    fix format

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-#generates a .tgz archive from the contents of the web_static folder
-
+# generates a .tgz archive from the contents of the web_static folder
 
 
 from fabric.api import *
@@ -12,8 +11,9 @@ from datetime import datetime
 def do_pack():
     """generate a .tgz archive"""
     t = datetime.now()
-    y,m,d,h,m,s = t.year,t.month,t.day,t.hour,t.minute,t.second
-    output_file = "versions/web_static_{}{}{}{}{}{}.tgz".format(y,m,d,h,m,s)
+    y, m, d, h, m, s = t.year, t.month, t.day, t.hour, t.minute, t.second
+    output_file = "versions/web_static_{}{}{}{}{}{}.tgz".\
+        format(y, m, d, h, m, s)
     if (not exists('versions')):
-    	mkdir('versions')
+        mkdir('versions')
     local("tar -zcvf {} web_static".format(output_file))
