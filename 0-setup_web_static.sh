adf5ec1195c8ccb992e5f218e829891a8532dba3
abelasfaw <abeloman89@gmail.com>
2022-12-25 17:18:11 +0000

    added task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#sets up your web servers for the deployment of web_static
+
+#install nginx
+apt-get -y update
+apt-get -y install nginx
+#allow Nginx
+ufw allow 'Nginx HTTP'
+#create project folders
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+#create a test page
+test_page="
+<html>
+   <head></head>
+   <body>Test Page</body>
+</html>
+"
+echo "$test_page" > /data/web_static/releases/test/index.html
+#create a symbolic link 
+ln -sf /data/web_static/releases/test /data/web_static/current
+#Give ownership of the /data/ folder to the ubuntu user and group
+chmod -R ubuntu:ubuntu /data
+#Update the Nginx configuration to serve the content of
+#/data/web_static/current/ to hbnb_static
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+#restart nginx
+service nginx restart
+exit
