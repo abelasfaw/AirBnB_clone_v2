385fe5063180bba320b4ebc83ead87823a7938df
abelasfaw <abeloman89@gmail.com>
2022-12-25 17:54:29 +0000

    use default file content for /etc/nginx/sites-available/default

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,20 +11,21 @@ mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 #create a test page
-test_page="
-<html>
-   <head></head>
-   <body>Test Page</body>
-</html>
-"
+test_page="<html>
+   <head>
+   </head>
+   <body>
+      Test Page
+   </body>
+</html>"
 echo "$test_page" > /data/web_static/releases/test/index.html
 #create a symbolic link 
 ln -sf /data/web_static/releases/test /data/web_static/current
 #Give ownership of the /data/ folder to the ubuntu user and group
-chmod -R ubuntu:ubuntu /data
+chown -R ubuntu:ubuntu /data
 #Update the Nginx configuration to serve the content of
 #/data/web_static/current/ to hbnb_static
-sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+sudo sed -i "/server_name _;/c\ \tserver_name _;\n\tlocation /hbnb_static {\n\t  alias /data/web_static/current/;\n\t  autoindex off;\n\t}" /etc/nginx/sites-available/default
 #restart nginx
 service nginx restart
 exit
