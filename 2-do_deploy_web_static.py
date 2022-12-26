0991879c67879dac54603f3b3abfd89810f73d30
abelasfaw <abeloman89@gmail.com>
2022-12-26 00:08:09 +0000

    fix task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -30,13 +30,13 @@ def do_deploy(archive_path):
     result = put(archive_path, remote_archive_path)
     if result.failed:
         return False
-    uncompressed_folder = "/data/web_static/releases/{}".
+    uncompressed_folder = "/data/web_static/releases/{}".\
     format(archive_path.split("/")[-1].split(".")[0])
     print("uncompressed folder: {}".format(uncompressed_folder))
     result = run("mkdir -p {}".format(uncompressed_folder))
     if result.failed:
         return False
-    result = run("tar -zxvf {} -C {}".
+    result = run("tar -zxvf {} -C {}".\
                  format(remote_archive_path, uncompressed_folder))
     if result.failed:
         return False
@@ -56,7 +56,7 @@ def do_deploy(archive_path):
     result = sudo("rm -rf /data/web_static/current")
     if result.failed:
         return False
-    result = sudo("ln -s {} /data/web_static/current".
+    result = sudo("ln -s {} /data/web_static/current".\
                   format(uncompressed_folder))
     if result.failed:
         return False
