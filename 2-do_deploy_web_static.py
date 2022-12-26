2876c04bac07f1149134c140b4ee0a9794243f68
abelasfaw <abeloman89@gmail.com>
2022-12-25 16:03:43 -0800

    fix format

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,6 +20,7 @@ def do_pack():
         mkdir('versions')
     local("tar -zcvf {} web_static".format(output_file))
 
+
 def do_deploy(archive_path):
     """distributes an archive to servers in env.hosts"""
     if (not exists(archive_path)):
@@ -29,12 +30,14 @@ def do_deploy(archive_path):
     result = put(archive_path, remote_archive_path)
     if result.failed:
         return False
-    uncompressed_folder ="/data/web_static/releases/{}".format(archive_path.split("/")[-1].split(".")[0])
+    uncompressed_folder = "/data/web_static/releases/{}".
+    format(archive_path.split("/")[-1].split(".")[0])
     print("uncompressed folder: {}".format(uncompressed_folder))
     result = run("mkdir -p {}".format(uncompressed_folder))
     if result.failed:
         return False
-    result = run("tar -zxvf {} -C {}".format(remote_archive_path, uncompressed_folder))
+    result = run("tar -zxvf {} -C {}".
+                 format(remote_archive_path, uncompressed_folder))
     if result.failed:
         return False
     result = sudo("rm -f {}".format(remote_archive_path))
@@ -44,17 +47,17 @@ def do_deploy(archive_path):
     print("uncompressed files: {}".format(uncompressed_files))
     result = sudo("mv {} {}".format(uncompressed_files, uncompressed_folder))
     if result.failed:
-    	return False
+        return False
     web_static_folder = "{}/web_static".format(uncompressed_folder)
     print("Web static folder: {}".format(web_static_folder))
     result = sudo("rm -rf {}".format(web_static_folder))
     if result.failed:
-    	return False
+        return False
     result = sudo("rm -rf /data/web_static/current")
     if result.failed:
-    	return False
-    result = sudo("ln -s {} /data/web_static/current".format(uncompressed_folder))
+        return False
+    result = sudo("ln -s {} /data/web_static/current".
+                  format(uncompressed_folder))
     if result.failed:
-    	return False
+        return False
     return True
-
