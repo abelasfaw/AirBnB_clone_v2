d86eda60f3d2c7aab36c48d2130bb3108d393249
abelasfaw <abeloman89@gmail.com>
2022-12-26 00:42:38 +0000

    added task 4

---
+++ 100-clean_web_static.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+# script that that deletes out-of-date archives
+from fabric.api import *
+from os.path import exists
+from os import mkdir
+from datetime import datetime
+
+
+env.hosts = ['54.167.86.129', '18.234.80.6']
+env.user = 'ubuntu'
+
+
+def do_pack():
+    """generate a .tgz archive"""
+    t = datetime.now()
+    y, m, d, h, m, s = t.year, t.month, t.day, t.hour, t.minute, t.second
+    output_file = "versions/web_static_{}{}{}{}{}{}.tgz".\
+        format(y, m, d, h, m, s)
+    if (not exists('versions')):
+        mkdir('versions')
+    local("tar -zcvf {} web_static".format(output_file))
+    return output_file
+
+def do_deploy(archive_path):
+    """distributes an archive to servers in env.hosts"""
+    if (not exists(archive_path)):
+        return False
+    remote_archive_path = "/tmp/{}".format(archive_path.split("/")[-1])
+    print("remote archive path: {}".format(remote_archive_path))
+    result = put(archive_path, remote_archive_path)
+    if result.failed:
+        return False
+    uncompressed_folder ="/data/web_static/releases/{}".format(archive_path.split("/")[-1].split(".")[0])
+    print("uncompressed folder: {}".format(uncompressed_folder))
+    result = run("mkdir -p {}".format(uncompressed_folder))
+    if result.failed:
+        return False
+    result = run("tar -zxvf {} -C {}".format(remote_archive_path, uncompressed_folder))
+    if result.failed:
+        return False
+    result = sudo("rm -f {}".format(remote_archive_path))
+    if result.failed:
+        return False
+    uncompressed_files = "{}/web_static/*".format(uncompressed_folder)
+    print("uncompressed files: {}".format(uncompressed_files))
+    result = sudo("mv {} {}".format(uncompressed_files, uncompressed_folder))
+    if result.failed:
+    	return False
+    web_static_folder = "{}/web_static".format(uncompressed_folder)
+    print("Web static folder: {}".format(web_static_folder))
+    result = sudo("rm -rf {}".format(web_static_folder))
+    if result.failed:
+    	return False
+    result = sudo("rm -rf /data/web_static/current")
+    if result.failed:
+    	return False
+    result = sudo("ln -s {} /data/web_static/current".format(uncompressed_folder))
+    if result.failed:
+    	return False
+    return True
+
+def deploy():
+    """deploy after archive"""
+    out = do_pack()
+    if (not exists(out)):
+    	return False
+    return do_deploy(out)
+
+def do_clean(number=0):
+    """Deletes out-of-date archives"""
+    archives = local("ls -1t versions", capture=True)
+    archive_names = archives.split("\n")
+    n = int(number)
+    if (n ==0 or n == 1):
+        n = 1
+    for i in archive_names[n:]:
+        local("rm versions/{}".format(i))
+    remote_dir = run("ls -1t /data/web_static/releases")
+    remote_dir_names = remote_dir.split("\n")
+    for folder in remote_dir_names[n:]:
+        if folder is 'test':
+            continue
+        run("rm -rf /data/web_static/releases/{}".format(folder))
