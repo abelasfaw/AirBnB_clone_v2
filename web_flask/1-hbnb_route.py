f27f0435df3e9196178ff284367c58bde75cd141
abelasfaw <abeloman89@gmail.com>
2023-01-17 13:15:44 +0000

    added task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""starts a flas web application on port 5000"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def root_handler():
+    """returns Hello HBNB! when request hits / route"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_handler():
+    """returns HBNB when request hits /hbnb route"""
+    return 'HBNB'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
